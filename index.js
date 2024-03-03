5a5275e9fac3248b39ff8d43134a0c8df8f892d9
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-03 22:24:18 +0100

    Completed (1 - 3): Odin Project Test Practice - Assignment

--- index.js
+++ index.js
@@ -2,9 +2,52 @@ function sum(a, b) {
     return a + b;
   }
 
-  function floatingPoint(firstNum, secondNum) {
+function floatingPoint(firstNum, secondNum) {
     return firstNum + secondNum;
   }
 
-  module.exports = floatingPoint;
-  module.exports = sum;
+
+// Odin Project Testing Practice Assignment
+
+function capitalize(string){
+  let firstChar = string[0]; 
+  firstChar = firstChar.toUpperCase();
+  let remainingString = string.slice(1);
+  let capString = firstChar + remainingString;
+   return capString;
+}
+
+function reverseString(reversedString){
+  return reversedString.split('').reverse().join('');
+}
+
+let calculator = {
+  
+  add: function(a, b){
+    return a + b;
+  },
+
+  multiply: function(a, b){
+    return a * b;
+  },
+
+  divide: function(a, b){
+    return a / b;
+  },
+
+  subtract: function(a, b){
+    return a - b;
+  }
+
+};
+
+
+
+
+module.exports = {
+  sum: sum,
+  floatingPoint: floatingPoint,
+  capitalize: capitalize,
+  reverseString: reverseString,
+  calculator: calculator
+};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const sum = require('./index');
-const val = require('./index');
+const { sum, floatingPoint, capitalize, reverseString, calculator } = require('./index');
+
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -7,5 +7,37 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('adding floating point numbers', () => {
     //expect(value).toBe(0.3);           This won't work because of rounding error
-    expect(val(0.1, 0.2)).toBeCloseTo(0.3);
+    expect(floatingPoint(0.1, 0.2)).toBeCloseTo(0.3);
+});
+
+
+// Odin Poject Testing Practice Assignment
+
+
+test('capitalized string', () => {
+  expect(capitalize('hackerstuff')).toMatch('Hackerstuff');
+});
+
+test('reversed string', () => {
+  expect(reverseString('counter')).toMatch('retnuoc');
+})
+
+describe('calculator', () => {
+  
+  test('added values', () => {
+    expect(calculator.add(5, 4)).toBe(9);
+  });
+
+  test('multipled values', () => {
+    expect(calculator.multiply(6, 6)).toBe(36);
+  });
+
+  test('divided values', () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+  });
+
+  test('subtract values', () => {
+    expect(calculator.subtract(535, 35)).toBe(500);
+  })
+
 });
