2537db1daaf96552ed39dc750408b123593c996a
Kaizen <125158784+kaizen011@users.noreply.github.com>
2024-03-05 22:15:18 +0100

    Untested logic (possibly incorrect)

--- index.js
+++ index.js
@@ -41,8 +41,66 @@ let calculator = {
 
 };
 
+function caesarCipher(word, shiftFactor){
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let i = 0;
+  let newWord = '';
+  let tempLet = '';
+  let punctuation = ['.', ',', '!', '?', ';', ':', '-', "'", '"', '(', ')', '[', ']', '{', '}', ' '];
+  let upperCaseLet = '';
+  let lowerCaseLet = '';
+
+  for(letter of word){
+
+    for(punki of punctuation){
+      if(letter == punki){
+      newWord += punki;
+      }
+    }
+    
+    
+    for(alfa of alphabet){
+      if(letter == letter.toUpperCase()) {
+
+        var index = alphabet.indexOf(alfa);
+
+        tempLet = alfa[index+shiftFactor];
+        upperCaseLet = tempLet.toUpperCase();
+        newWord = newWord + upperCaseLet;
+
+      } else {
+        tempLet = alfa[index+shiftFactor];
+        lowerCaseLet = tempLet.toUpperCase();
+        newWord = newWord + lowerCaseLet;
+      }
+    }
 
 
+
+    
+    }
+  
+  }
+
+    // if(letter == alphabet[i]){
+
+    //   for(i; i < alphabet.length(); i++){
+    //      if(letter == letter.toUpperCase()) {
+         
+    //       tempLet = alphabet[i+shiftFactor];
+    //       newWord = newWord + tempLet.toUpperCase();
+    //       tempLet = '';
+
+    //     } else {
+    //       tempLet = alphabet[i+shiftFactor];
+    //       newWord = newWord + tempLet.toLocaleLowerCase();
+    //       tempLet = '';
+    //     }
+         
+    //   }
+      
+    //  }
+  
 
 module.exports = {
   sum: sum,
